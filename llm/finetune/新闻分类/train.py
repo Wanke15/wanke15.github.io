f520a153c03a9eae67966dc0504a474a6dd8b601
Jeff <1015005300@qq.com>
2025-04-14 14:20:30 +0800

    Create train.py

---
+++ llm/finetune/新闻分类/train.py
@@ -0,0 +1,125 @@
+import torch
+import pandas as pd
+from datasets import Dataset
+
+from modelscope import AutoTokenizer
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq
+from peft import LoraConfig, TaskType, get_peft_model
+
+def process_func(example):
+    """
+    将数据集进行预处理
+    """
+    MAX_LENGTH = 384
+    input_ids, attention_mask, labels = [], [], []
+    instruction = tokenizer(
+        f"<|im_start|>system\n你是一个文本分类领域的专家，你会接收到一段文本和几个潜在的分类选项，请输出文本内容的正确类型<|im_end|>\n<|im_start|>user\n{example['input']}<|im_end|>\n<|im_start|>assistant\n",
+        add_special_tokens=False,
+    )
+    response = tokenizer(f"{example['output']}", add_special_tokens=False)
+    input_ids = instruction["input_ids"] + response["input_ids"] + [tokenizer.pad_token_id]
+    attention_mask = (
+            instruction["attention_mask"] + response["attention_mask"] + [1]
+    )
+    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"] + [tokenizer.pad_token_id]
+    if len(input_ids) > MAX_LENGTH:  # 做一个截断
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
+
+
+def predict(messages, model, tokenizer):
+    device = "cpu"
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return response
+
+
+# 在modelscope上下载Qwen模型到本地目录下
+# model_dir = snapshot_download("Qwen/Qwen2.5-0.5B", cache_dir="/data/llm/modelscope", revision="master")
+model_dir = '/data/llm/modelscope/Qwen/Qwen2___5-0___5B'
+
+# 数据和模型路径
+train_jsonl_new_path = "output/data/train.jsonl"
+test_jsonl_new_path = "output/data/test.jsonl"
+sft_model_save_dir = "output/model/Qwen25_05b_fudan_news"
+
+# Transformers加载模型权重
+tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", torch_dtype=torch.bfloat16)
+model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
+
+train_df = pd.read_json(train_jsonl_new_path, lines=True).head(100)
+train_ds = Dataset.from_pandas(train_df)
+train_dataset = train_ds.map(process_func, remove_columns=train_ds.column_names)
+print(train_dataset)
+
+config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+    inference_mode=False,  # 训练模式
+    r=8,  # Lora 秩
+    lora_alpha=32,  # Lora alaph，具体作用参见 Lora 原理
+    lora_dropout=0.1,  # Dropout 比例
+)
+model = get_peft_model(model, config)
+
+args = TrainingArguments(
+    output_dir=sft_model_save_dir,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    logging_steps=10,
+    num_train_epochs=2,
+    save_steps=100,
+    learning_rate=1e-4,
+    save_on_each_node=True,
+    gradient_checkpointing=True,
+    report_to="none",
+)
+
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
+)
+
+trainer.train()
+
+
+# 用测试集的前10条，测试模型
+test_df = pd.read_json(test_jsonl_new_path, lines=True)[:10]
+
+
+test_text_list = []
+for index, row in test_df.iterrows():
+    instruction = row['instruction']
+    input_value = row['input']
+
+    messages = [
+        {"role": "system", "content": f"{instruction}"},
+        {"role": "user", "content": f"{input_value}"}
+    ]
+
+    response = predict(messages, model, tokenizer)
+    print("#####", messages, response)
+    messages.append({"role": "assistant", "content": f"{response}"})
+    result_text = f"{messages[0]}\n\n{messages[1]}\n\n{messages[2]}"
+    test_text_list.append(result_text)
+
+print(test_text_list)
